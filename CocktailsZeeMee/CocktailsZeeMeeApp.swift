e0af5e27a74eaeac61280b9bdd0afdbcd0aa98e1
Kostiantyn Nevinchanyi <konstantin.nevinchanyi@bambuser.com>
2023-04-01 15:07:44 -0400

    Initial Commit

---
+++ CocktailsZeeMee/CocktailsZeeMeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsZeeMeeApp.swift
+//  CocktailsZeeMee
+//
+//  Created by Kostiantyn Nevinchanyi on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CocktailsZeeMeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
